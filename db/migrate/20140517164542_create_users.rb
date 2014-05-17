5a0bceb55be5006e550793ac78f8873c012017cc
ubuntu <shamil.ishraq@gmail.com>
2014-05-17 14:51:48 -0400

    User table

--- db/migrate/20140517164542_create_users.rb
+++ db/migrate/20140517164542_create_users.rb
@@ -1,11 +1,15 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :first_name
-      t.string :last_name
-
-
+      t.string :first_name, limit: 100, null: false
+      t.string :last_name, limit: 100, null: false
+      t.string :avatar_url
+      t.string :email, limit: 100, null: false
+      t.string :password, limit: 50, null: false
       t.timestamps
     end
+    add_index :users, :first_name
+    add_index :users, :last_name
+    add_index :users, :email
   end
 end
