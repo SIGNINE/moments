20083f6cc2cfffba5702f1fef6d34c4efa4f8ef4
Naeem Talukdar <naeem.talukdar@gmail.com>
2014-05-18 17:56:23 -0400

    AUTOCOMMIT

--- public/assets/js/views/home/HomeView.js
+++ public/assets/js/views/home/HomeView.js
@@ -2,29 +2,22 @@ define([
   'jquery',
   'underscore',
   'backbone',
-  'text!templates/home/homeTemplate.html',
-  'models/user/UserModel'
-], function($, _, Backbone, UserModel, homeTemplate){
+  'text!templates/home/homeTemplate.html'
+], function($, _, Backbone, homeTemplate){
 
   var HomeView = Backbone.View.extend({
     el: $("#container"),
 
     events: {
-      'click input#btn_register': 'newUser'
+      'click input#btn_register': 'register'
     },
 
-    newUser:function(e){
-      e.preventDefault();
+    initialize: function(){
+      _.bindAll(this, 'submit');
+    },
+
+    register:function(e){
       alert("hi");
-      var user = new UserModel({ first_name: 'naeem', last_name: 'talukdar', email: 'nt', password: 'lk', type: 'login'});
-        user.save({
-          success: function(model, resp){
-            alert("wel done!");
-          },
-          error: function(){
-            alert("aw we failed :(");
-          }
-        });
     },
 
     render: function(){
