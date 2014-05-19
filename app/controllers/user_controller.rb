2d435a96bb862711409191799b736fcd3f55857e
ubuntu <shamil.ishraq@gmail.com>
2014-05-19 11:22:24 -0400

    login

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,28 @@
 require 'digest/sha1'
 
 class UserController < ApplicationController
+  before_fiter :not_authenticated, only: [:login]
+
+  def login
+    respond_to do |format|
+      format.json do
+        return unless validate_params [:email, :password]
+
+        user = User.find_by_email params[:email]
+        if user == nil
+          render json: { status: 400,  error: "User with email: #{params[:email]} does not exit" }, status: 400 
+          return
+        end
+
+        if password_matches? user.password, params[:password]
+          register_session user.id
+          render json: { status: 200 }
+        else
+          render json: { status: 401, error: "Wrong password" }, status: 401
+        end
+
+      end
+  end
 
   # register
   def create
@@ -45,9 +67,53 @@ class UserController < ApplicationController
     salt + hashed
   end
 
+  def password_matches? user_pass, provided_pass
+    salt = user_pass[0..9]
+    hashed_pass = Digest::SHA1.base64digest(provided_pass + salt)
+
+    user_pass == hashed_pass
+  end
+
   def generate_salt
+    random_str 10
+  end
+
+  def generate_session
+    random_str 10
+  end
+
+  def random_str size
     o = [('a'..'z'), ('A'..'Z'), (1..9)].map { |i| i.to_a }.flatten
     o = o.map { o[rand(o.length)] }
-    (0..9).map { o[rand(o.length)] }.join
+    (1..size).map { o[rand(o.length)] }.join
   end
-end
+
+  def find_session id
+    $redis.get("user-session-*-#{id}")
+  end
+
+  def register_session user_id
+    # delete all sessions linked to this user
+    $redis.keys("user-session-#{user_id}-*").each { |k| $redis.del(k) }
+    
+    session_id = generate_session
+    key = "user-session-#{user_id}-#{session_id}"
+    $redis.set(key, user_id)
+    $redis.expire(key, 24*60*60)
+
+    session[:session_id] = session_id
+  end
+
+  def not_authenticated
+    session_id = session[:session_id]
+    return if session_id == nil || sessn_id == ''
+
+    user_id = find_session session_id
+    return if user_id == nil
+ 
+    respond_to do |f|
+      f.json { render json: { status: 401, error: "User already logged in" }, status: 404 }
+    end
+  end
+
+ end
