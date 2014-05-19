ada38813f37437db782ff3c17e46f5c62f790e76
Naeem Talukdar <naeem.talukdar@gmail.com>
2014-05-18 20:35:18 -0400

    AUTOCOMMIT

--- public/assets/js/views/home/HomeView.js
+++ public/assets/js/views/home/HomeView.js
@@ -13,7 +13,6 @@ define([
     },
 
     register:function(e){
-      $("button#btn_register").
       $email = $("button#btn_register").parent().find("input:text").val();
       $pw = $("button#btn_register").parent().find("input:password").val();
       this.model.set({"email":$email,"password":$pw});
