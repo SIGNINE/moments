b9b782d407689e922ce08cf5cd40f028e91dd618
Naeem Talukdar <naeem.talukdar@gmail.com>
2014-05-19 12:31:52 -0400

    AUTOCOMMIT

--- public/assets/js/router.js
+++ public/assets/js/router.js
@@ -19,8 +19,15 @@ define([
       // Default
       '*actions': 'defaultAction'
     },
-    alertTime: function(){
-      alert("hi");
+    getCookie: function(name){
+      var nameEQ = name + "=";
+      var ca = document.cookie.split(';');
+      for(var i=0;i < ca.length;i++) {
+          var c = ca[i];
+          while (c.charAt(0)==' ') c = c.substring(1,c.length);
+          if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+      }
+      return null;
     }
   });
   
@@ -28,9 +35,16 @@ define([
 
     window.app_router = new AppRouter;
 
+    $.ajaxSend(function(event, request) {
+      var token = app_router.getCookie("session_id");
+      if (token) {
+        request.setRequestHeader("session_id", token);
+      }
+    });
+
     app_router.on('route:showPhotos', function(){
 
-        app_router.alertTime();
+        
         alert("testing");
         // Call render on the module we loaded in via the dependency array
         if(!$("#contents").is(":visible"))
