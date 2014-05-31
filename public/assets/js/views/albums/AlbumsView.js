62bf6e75dd96635ffb8ebae7d630fc0b29f28e09
Naeem Talukdar <naeem.talukdar@gmail.com>
2014-05-31 16:18:49 -0400

    AUTOCOMMIT

--- public/assets/js/views/albums/AlbumsView.js
+++ public/assets/js/views/albums/AlbumsView.js
@@ -10,22 +10,11 @@ define([
 
   var AlbumsView = Backbone.View.extend({
     el: $("#page"),
-
     render: function(){
-      // $('.menu li').removeClass('active');
-      // $('.menu li a[href="'+window.location.hash+'"]').parent().addClass('active');
-      // this.$el.hide();
-      // this.$el.html(albumsTemplate).fadeIn();
-
-      $.ajax({
-        url: "/user/"+window.user_id+'/album',
-        type: "POST",
-        data: "title='testing';session_id="+window.session_id,
-        success:function(result){
-          alert('succes');
-          window.rrr = result;
-        }
-      });
+      $('.menu li').removeClass('active');
+      $('.menu li a[href="'+window.location.hash+'"]').parent().addClass('active');
+      this.$el.hide();
+      this.$el.html(albumsTemplate).fadeIn();
 
       var photo1 = new AlbumsModel({title:'Album 1', url: 'http://i.imgur.com/vSRCLfM.jpg'}); 
       var photo2 = new AlbumsModel({title:'Album 2', url: 'http://i.imgur.com/vSRCLfM.jpg'}); 
@@ -33,18 +22,14 @@ define([
       var photo4 = new AlbumsModel({title:'Album 4', url: 'http://i.imgur.com/vSRCLfM.jpg'});
       var photo5 = new AlbumsModel({title:'Album 5', url: 'http://i.imgur.com/vSRCLfM.jpg'});
 
-      var aPhotos = [photo1,photo2,photo3,photo4,photo5];
+      var aPhotos = [photo1, 
+                      photo2,
+                      photo3,
+                      photo4,
+                      photo5];
 
-      window.ap = aPhotos;
-      var pc = new AlbumsCollection(aPhotos);
-      window.app = pc;
-
-      var photosCollection = new AlbumsCollection();
-      photosCollection.put("user_id",window.user_id);
-      photosCollection.put("session_id",window.session_id); 
-      photosCollection.fetch(); 
-      window.p = photosCollection;
-      var photosListView = new AlbumsListView({ collection: pc}); 
+      var photosCollection = new AlbumsCollection(aPhotos);  
+      var photosListView = new AlbumsListView({ collection: photosCollection}); 
       
       photosListView.render(); 
 
