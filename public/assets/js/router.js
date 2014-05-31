934fc00fd519203aa40a0ac338e1a2e3fa30affd
Naeem Talukdar <naeem.talukdar@gmail.com>
2014-05-31 10:35:00 -0400

    Revert "Revert "Revert 38ffe20..92a2a2a""
    This reverts commit 44860992933a817164164b294a9b15300d45a9d5.

--- public/assets/js/router.js
+++ public/assets/js/router.js
@@ -73,7 +73,7 @@ define([
 
     app_router.on('route:loginUser', function(){
    
-        var user = new UserModel({ first_name: 'naeem', last_name: 'talukdar', email: 'nt', password: 'lk'});
+        var user = new UserModel({ first_name: 'naeem', last_name: 'talukdar', email: 'nt', password: 'lk', type: 'login'});
         user.save({}, {
           success: function(model, resp){
             alert("wel done!");
