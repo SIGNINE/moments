07481388dfa5319001821e8a5548f6525c7f9001
Naeem Talukdar <naeem.talukdar@gmail.com>
2014-05-31 16:00:41 -0400

    AUTOCOMMIT

--- public/assets/js/views/albums/AlbumsView.js
+++ public/assets/js/views/albums/AlbumsView.js
@@ -40,7 +40,7 @@ define([
       photosCollection.put("user_id",window.user_id);
       photosCollection.put("session_id",window.session_id); 
       photosCollection.fetch(); 
-      
+      window.p = photosCollection;
       var photosListView = new AlbumsListView({ collection: photosCollection}); 
       
       photosListView.render(); 
