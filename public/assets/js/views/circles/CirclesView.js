537b1ceb9f264660b6f1095c989d40aeeb0dbfb8
Naeem Talukdar <naeem.talukdar@gmail.com>
2014-05-31 10:53:04 -0400

    Revert 2713e9d..4c693e5
    This rolls back to commit 2713e9d0d07468d19bb9b0f671d47fe2df72a1f8.

--- public/assets/js/views/circles/CirclesView.js
+++ public/assets/js/views/circles/CirclesView.js
@@ -2,21 +2,39 @@ define([
   'jquery',
   'underscore',
   'backbone',
+  'models/circles/CirclesModel',
+  'collections/circles/CirclesCollection',
+  'views/circles/CirclesListView',
   'text!templates/circles/circlesTemplate.html'
-], function($, _, Backbone, circlesTemplate){
+], function($, _, Backbone, CirclesModel, CirclesCollection, CirclesListView, circlesTemplate){
 
   var CirclesView = Backbone.View.extend({
     el: $("#page"),
-
     render: function(){
-      
-      this.$el.hide();
-      this.$el.hide().html(circlesTemplate).fadeIn();
- 
-    }
 
+      this.$el.hide();
+      this.$el.html(photosTemplate).fadeIn();
+
+      var photo1 = new CirclesModel({title:'Photo 1', url: 'http://i.imgur.com/ODT5hTG.png?1'}); 
+      var photo2 = new CirclesModel({title:'Photo 2', url: 'http://i.imgur.com/ODT5hTG.png?1'}); 
+      var photo3 = new CirclesModel({title:'Photo 3', url: 'http://i.imgur.com/ODT5hTG.png?1'}); 
+      var photo4 = new CirclesModel({title:'Photo 4', url: 'http://i.imgur.com/ODT5hTG.png?1'});
+      var photo5 = new CirclesModel({title:'Photo 5', url: 'http://i.imgur.com/ODT5hTG.png?1'});
+
+      var aPhotos = [photo1, 
+                      photo2,
+                      photo3,
+                      photo4,
+                      photo5];
+
+      var circlesCollection = new CirclesCollection(aPhotos);  
+      var circlesListView = new CirclesListView({ collection: circlesCollection}); 
+      
+      circlesListView.render(); 
+
+
+    }
   });
 
   return CirclesView;
-  
 });
