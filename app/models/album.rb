0aa810fdf5b182dfbea81fc9cdd9da61dc55f1fd
ubuntu <shamil.ishraq@gmail.com>
2014-05-31 15:14:51 -0400

    album index and create

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,5 @@
 class Album < ActiveRecord::Base
-  validates :name, presence: true
+  validates :title, presence: true
 
   belongs_to :user, dependent: :destroy
   has_and_belongs_to_many :photos 
