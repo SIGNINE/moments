a68206bc13b8766b9b2b45c6676958e03db92b81
Naeem Talukdar <naeem.talukdar@gmail.com>
2014-05-31 11:51:55 -0400

    AUTOCOMMIT

--- public/assets/js/views/home/HomeView.js
+++ public/assets/js/views/home/HomeView.js
@@ -22,6 +22,7 @@ define([
       var user = new UserModel({ email: $email, password: $pw});
       user.save({}, {
         success: function(model, resp){
+          window.r = resp;
           document.cookie='session_id='+window.r.session_id+';expires='+(new Date(new Date().getTime()+86400000).toGMTString())+';path=/';
           window.location= '#circles';
           window.location.reload();
