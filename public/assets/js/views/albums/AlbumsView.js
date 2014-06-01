3b58198307670c461a18fdcd20fd3928e638cf39
Naeem Talukdar <naeem.talukdar@gmail.com>
2014-06-01 11:44:55 -0400

    AUTOCOMMIT

--- public/assets/js/views/albums/AlbumsView.js
+++ public/assets/js/views/albums/AlbumsView.js
@@ -21,41 +21,26 @@ define([
       album.save({session_id: window.session_id, title: $title}, {
         success: function(model, resp){
           if(resp.status == 200)
-            alert('success')
+            this.render;
           else
             alert('failed')
         },
         error: function(model, resp){
           if(resp.status == 200)
-            alert('success')
+            this.render;
           else
             alert('failed')
         }
       });
     },
     render: function(){
-
+      console.log("Rendering...");
       //This stuff is required for some reason
       $('.menu li').removeClass('active');
       $('.menu li a[href="'+window.location.hash+'"]').parent().addClass('active');
       this.$el.hide();
       this.$el.html(albumsTemplate).fadeIn();
 
-
-      var photo1 = new AlbumsModel({title:'Album 1', url: 'http://i.imgur.com/vSRCLfM.jpg'}); 
-      var photo2 = new AlbumsModel({title:'Album 2', url: 'http://i.imgur.com/vSRCLfM.jpg'}); 
-      var photo3 = new AlbumsModel({title:'Album 3', url: 'http://i.imgur.com/vSRCLfM.jpg'}); 
-      var photo4 = new AlbumsModel({title:'Album 4', url: 'http://i.imgur.com/vSRCLfM.jpg'});
-      var photo5 = new AlbumsModel({title:'Album 5', url: 'http://i.imgur.com/vSRCLfM.jpg'});
-
-      var aPhotos = [photo1, 
-                      photo2,
-                      photo3,
-                      photo4,
-                      photo5];
-
-      var photosCollection = new AlbumsCollection(aPhotos);  
-      console.log(photosCollection);
       var aC = new AlbumsCollection();
       aC.put("user_id",window.user_id);
       aC.put("session_id",window.session_id); 
