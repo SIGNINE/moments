965033dbc977b62207fb45a5c60b4a5d7331b068
Naeem Talukdar <naeem.talukdar@gmail.com>
2014-06-01 11:36:01 -0400

    AUTOCOMMIT

--- public/assets/js/views/albums/AlbumsView.js
+++ public/assets/js/views/albums/AlbumsView.js
@@ -41,13 +41,28 @@ define([
       this.$el.hide();
       this.$el.html(albumsTemplate).fadeIn();
 
+
+      var photo1 = new AlbumsModel({title:'Album 1', url: 'http://i.imgur.com/vSRCLfM.jpg'}); 
+      var photo2 = new AlbumsModel({title:'Album 2', url: 'http://i.imgur.com/vSRCLfM.jpg'}); 
+      var photo3 = new AlbumsModel({title:'Album 3', url: 'http://i.imgur.com/vSRCLfM.jpg'}); 
+      var photo4 = new AlbumsModel({title:'Album 4', url: 'http://i.imgur.com/vSRCLfM.jpg'});
+      var photo5 = new AlbumsModel({title:'Album 5', url: 'http://i.imgur.com/vSRCLfM.jpg'});
+
+      var aPhotos = [photo1, 
+                      photo2,
+                      photo3,
+                      photo4,
+                      photo5];
+
+      var photosCollection = new AlbumsCollection(aPhotos);  
+      console.log(photosCollection);
       var aC = new AlbumsCollection();
       aC.put("user_id",window.user_id);
       aC.put("session_id",window.session_id); 
       aC.fetch({data: {session_id: window.session_id}, type: 'GET'}); 
       window.ac = aC;
-      
-      var albumListView = new AlbumsListView({ collection: aC}); 
+      console.log(aC);
+      var albumListView = new AlbumsListView({ collection: photosCollection}); 
       
       albumListView.render(); 
 
