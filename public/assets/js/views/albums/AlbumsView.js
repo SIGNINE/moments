f3ab408dbbc223f555df42d489c7d1d440f4d6a5
Naeem Talukdar <naeem.talukdar@gmail.com>
2014-06-01 11:40:09 -0400

    AUTOCOMMIT

--- public/assets/js/views/albums/AlbumsView.js
+++ public/assets/js/views/albums/AlbumsView.js
@@ -59,7 +59,7 @@ define([
       var aC = new AlbumsCollection();
       aC.put("user_id",window.user_id);
       aC.put("session_id",window.session_id); 
-      aC.fetch({data: {session_id: window.session_id}, type: 'GET'}); 
+      aC.fetch({data: {session_id: window.session_id}, type: 'GET'}, success: function(results){alert('hi'); window.ac = aC;}); 
       window.ac = aC;
       console.log(aC);
       var albumListView = new AlbumsListView({ collection: photosCollection}); 
