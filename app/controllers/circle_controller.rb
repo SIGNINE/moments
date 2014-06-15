b3deba8abb6e97eb7d8259277d718a0efdeac50b
ubuntu <shamil.ishraq@gmail.com>
2014-06-15 10:43:18 -0400

    circles/:id/albums

--- app/controllers/circle_controller.rb
+++ app/controllers/circle_controller.rb
@@ -1,5 +1,5 @@
 class CircleController < ApplicationController
-  before_filter :authenticate, only: [:index, :create, :add_user]
+  before_filter :authenticate, only: [:index, :create, :add_user, :add_album, :get_albums]
 
   def index
     respond_to do |f|
@@ -72,4 +72,61 @@ class CircleController < ApplicationController
       end
     end
 
+    def add_album
+      respond_to do |f|
+        f.json do 
+          return unless validate_params [:id, :user_id, :album_id]
+
+          @circle = Circle.find_by_id params[:id]
+          unless @circle
+            render json: {status: 422, error: "Circle with id: #{params[:id]} does not exist"}, status: 422
+            return
+          end
+
+          unless @circle.users.include? @user
+            render json: { status: 401, error: "User does not have permission" }, status: 401
+            return
+          end
+
+          @album = Album.find_by_id params[:album_id]
+          unless @album
+            render json: {status: 422, error: "Album with id: #{params[:album_id]} does not exist"}, status: 422
+            return
+          end
+
+          unless @user.albums.include? @album 
+            render json: {status: 422, error: "Album does not belong to user"}, status: 422
+            return
+          end
+
+          if @circle.albums.include? @album
+            render json: {status: 422, error: "Album with id: #{params[:album_id]} already belongs to circle id: #{params[:id]}"}, status: 422
+            return
+          end
+
+          @circle.albums << @albums
+          render json: {status: 200}
+        end
+      end
+    end
+
+    def get_albums
+      respond_to do |f|
+        f.json do
+          @circle = Circle.find_by_id params[:id]
+
+          unless @circle
+            render json: {status: 404, error: "Circle with id: #{params[:id]} does not exist"}, status: 404
+            return
+          end
+
+          unless @circle.users.include? @user
+            render json: {status: 403, error: "User does not have permission"}, status: 403
+            return
+          end
+
+          @albums = @circle.albums.map { |a| {"id": a.id, "title": a.title} }
+        end
+      end
+    end
 end
