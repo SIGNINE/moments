993aa6e9523feb8da6272ad3a3163fbda2b6f8f0
Naeem Talukdar <naeem.talukdar@gmail.com>
2014-06-15 11:53:24 -0400

    AUTOCOMMIT

--- public/assets/js/views/albums/AlbumsView.js
+++ public/assets/js/views/albums/AlbumsView.js
@@ -37,14 +37,15 @@ define([
       $title = $("#submit_album").parent().find("input:text").val();
       album.save({session_id: window.session_id, title: $title}, {
         success: function(model, resp){
-          if(resp.status == 200)
+          if(resp.status == 200){
             window.View.render();
             console.log(resp);
             console.log(model);
             alert(resp);
             alert(model);
-          else
+          }else{
             alert('failed');
+          }
         },
         error: function(model, resp){
           if(resp.status == 200)
